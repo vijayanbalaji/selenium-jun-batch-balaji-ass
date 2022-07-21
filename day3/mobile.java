2c292fc8223be9ffa86e7df405245e2b42799050
vijayanbalaji <107264361+vijayanbalaji@users.noreply.github.com>
2022-07-21 18:05:14 +0530

    Add files via upload

---
+++ day3/mobile.java
@@ -0,0 +1,23 @@
+package week1.day3;
+
+public class mobile {
+	int ramSize=16;
+	long imiNumber=1999999999999l;
+	boolean isCharged=true;
+	String brandName="redmi";
+	String modelName="note9";
+	double mobilePrice=14000.30d;
+	
+	public static void main(String[] args) {
+		mobile m=new mobile();
+		System.out.println(m.ramSize);
+		System.out.println(m.imiNumber);
+		System.out.println(m.isCharged);
+		System.out.println(m.brandName);
+		System.out.println(m.modelName);
+		System.out.println(m.mobilePrice);
+
+	}
+	
+}
+	
---
+++ day3/whatsApp.java
@@ -0,0 +1,24 @@
+package week1.day3;
+
+public class whatsApp {
+	private void makeWhatsAppCall() {
+		// TODO Auto-generated method stub
+System.out.println("whatsapp call");
+	}
+	public void whatsAppChat() {
+		// TODO Auto-generated method stub
+System.out.println("WHATS APP CHAT");
+	}
+	void whatsAppDp() {
+		// TODO Auto-generated method stub
+		System.out.println("WHATS APP dp");
+
+	}
+
+	public static void main(String[] args) {
+		whatsApp wa=
+		// TODO Auto-generated method stub
+
+	}
+
+}
